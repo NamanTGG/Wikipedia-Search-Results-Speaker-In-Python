c946a52784d8a37fa7bf69fc7a35c13ed4fc609a
NamanTGG <67754467+NamanTGG@users.noreply.github.com>
2020-09-23 13:12:56 +0530

    Update Wikipedia Search Result Speaker .py

--- Wikipedia Search Result Speaker .py
+++ Wikipedia Search Result Speaker .py
@@ -1,3 +1,4 @@
+# THE PROGRAM MIGHT TAKE A FEW MINUTES TO RUN OR SOMETIMES A FEW SECONDS - IT DEPENDS ON YOUR COMPUTER'S SPEED 
 import pyttsx3
 import wikipedia
 
@@ -9,7 +10,7 @@ def speak(message):
     engine.runAndWait()
 
 def my_summary():
-    summ=wikipedia.summary('')
+    summ=wikipedia.summary('TYPE IN YOUR WHAT YOU WANT TO SEARCH HERE - NOTE - THE TOPIC MUST BE DOCUMENTED  BY WIKIPEDIA FOR THE PROGRAM TO WORK  ')
     return summ
  
 val = my_summary()
